c4a9f01e2278173a8c78307a42ff561f70ada769
luobinbin <luobinb7@gmail.com>
2023-05-15 17:29:57 +0800

    add_Car.vue

---
+++ vue_pro/src/components/Car.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>品牌{{ brand }}</h3>
+    <h3>价格{{ price }}</h3>
+    <h3>颜色{{ color }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            brand : 'BMWR8',
+            price : '20',
+            color : 'schwarz'
+        }
+    }
+
+}
+</script>

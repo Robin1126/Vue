67caed988934d65fff26020d1151667533e64860
luobinbin <luobinb7@gmail.com>
2023-05-17 09:02:53 +0800

    配置props属性，父组件传入参数

--- vue_pro/src/App.vue
+++ vue_pro/src/App.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
-        <h1>App组件</h1>
-        <Car>汽车</Car>
+        <h1>测试自己的脚手架挂件</h1>
+        <hr>
+        <h1>{{ msg }}</h1>
+        <hr>
+        <!-- 父组件中可以传参数给子组件 -->
+        <!-- 使用v-bind将price后面的改成数字，bind后面可以跟表达式，常量等 -->
+        <Car brand="奔驰320" color="红色" :price="10"></Car>
+        <hr>
+        <Car brand="宝马520" color="黑色" :price="20"></Car>
+        <hr>
+        <Car brand="特斯拉Model-M" :price="20"></Car>
+       
     </div>
    
 </template>
@@ -9,6 +19,12 @@
 <script>
 import Car from './components/Car.vue'
 export default {
+    name : 'App',
+    data() {
+        return {
+            msg : '汽车信息'
+        }
+    },
     components : {Car}
 }
 </script>
--- vue_pro/src/components/Car.vue
+++ vue_pro/src/components/Car.vue
@@ -1,20 +1,67 @@
 <template>
   <div>
-    <h3>品牌{{ brand }}</h3>
-    <h3>价格{{ price }}</h3>
-    <h3>颜色{{ color }}</h3>
+    <h3>品牌: {{ brand }}</h3>
+    <h3>价格: {{ cprice }}</h3>
+    <h3>颜色: {{ color }}</h3>
+    <button @click="add">价格+1</button>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
+  name : 'Car',
+  data() {
+    return {
+       cprice : this.price
+    }
+  },
+  methods : {
+      add() {
+        this.cprice++
+      }
+  },
+
+  /*   data() {
         return {
             brand : 'BMWR8',
             price : '20',
             color : 'schwarz'
         }
+    } */
+    
+    // 接受从父组件中传过来的参数,实现动态传参
+
+    // 第一种接收方式：采用数组接收
+    // props : ['brand','color','price']
+
+    // 第二种接收方式：带有限定格式
+/*     props : {
+        brand : String,
+        color : String,
+        price : Number // 要传入数字
+    } */
+
+    // 第三种接收方式：添加限定格式，默认值，添加必要性
+    // 注意：避免直接修改props当中的值，因为每次页面渲染的时候都会重新赋值
+    // 可以考虑使用中间变量来变动相应的值，比如汽车价格
+    props : {
+      brand : {
+        type : String,
+        required : true
+        // 表示这个属性是必须的，否则报错
+      },
+      color : {
+        type : String,
+        default : '红色'
+        // 如果这个属性不传入参数，默认是红色
+      },
+      price : {
+        type : Number,
+        required : true
+      }
+
     }
+
 
 }
 </script>
